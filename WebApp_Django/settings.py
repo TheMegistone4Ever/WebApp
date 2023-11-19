bbe9e0fa92f0f986cc695737607bda84f817da5c
MYKYTA\megis <zeusmobilenick@gmail.com>
2023-11-20 01:48:44 +0200

    Lesson 1. Launched Home page successfully...

--- WebApp_Django/settings.py
+++ WebApp_Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r%z+&%g_vdt_@ps3i)1x(2py$l$q3%ri!e_gp6qd=u#-m&im8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
